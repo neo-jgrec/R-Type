7b03400b98f3db6a687712a51fa24bb0430dc6eb
tom <tom.lefoix@epitech.eu>
2024-09-23 17:49:44 +0200

    feat(feature/nertworking-service): add dynamic handler for payload

--- src/core/network/NetworkService.hpp
+++ src/core/network/NetworkService.hpp
@@ -17,6 +17,7 @@
     #include <map>
     #include "./includes/RequestType.hpp"
     #include "./includes/RequestHeader.hpp"
+#include "../game/PreparePayload.hpp"
 
 
 /**
@@ -33,13 +34,15 @@ public:
     * @param server_ip The IP address of the server.
     * @param port The port number to listen on.
     * @param message_handlers A shared pointer to a map of message handlers for different GDTPMessageType.
+    * @param payload_handlers A shared pointer to a map of payload handlers for different GDTPMessageType.
     */
     NetworkingService(
         const std::string& server_ip,
         const int port,
-        const std::shared_ptr<std::map<GDTPMessageType, std::function<void(const GDTPHeader& header, const std::vector<uint8_t>& payload, const asio::ip::udp::endpoint& client_endpoint)>>>& message_handlers = std::make_shared<std::map<GDTPMessageType, std::function<void(const GDTPHeader& header, const std::vector<uint8_t>& payload, const asio::ip::udp::endpoint& client_endpoint)>>>()
+        const std::shared_ptr<std::map<GDTPMessageType, std::function<void(const GDTPHeader& header, const std::vector<uint8_t>& payload, const asio::ip::udp::endpoint& client_endpoint)>>>& message_handlers = std::make_shared<std::map<GDTPMessageType, std::function<void(const GDTPHeader& header, const std::vector<uint8_t>& payload, const asio::ip::udp::endpoint& client_endpoint)>>>(),
+        const std::shared_ptr<std::map<GDTPMessageType, std::function<std::vector<uint8_t>(std::vector<std::string>)>>>& payload_handlers = Payload::payloadMap()
     )
-        : socket_(io_context_, asio::ip::udp::endpoint(asio::ip::udp::v4(), port)), message_handlers(message_handlers) {
+        : socket_(io_context_, asio::ip::udp::endpoint(asio::ip::udp::v4(), port)), message_handlers(message_handlers), payload_handlers(payload_handlers) {
         std::cout << "Server listening on " << server_ip << ":" << port << std::endl;
         startReceive(); // Commence l'écoute des paquets dès l'initialisation
     }
@@ -153,6 +156,24 @@ public:
         thread.join();
     }
 
+    //create errror for unkown payload message
+    /**
+     * @brief Exception class for handling unknown payload messages.
+     */
+    class UnknownPayloadMessage : public std::exception {
+        public:
+            UnknownPayloadMessage() = delete;
+            UnknownPayloadMessage(GDTPMessageType messageType) : messageType(messageType)
+            {
+
+            }
+            const char *what() const noexcept override
+            {
+                return "Payload Message handlers is missing for this message type: " + static_cast<int>(this->messageType);
+            };
+        private:
+            GDTPMessageType messageType;
+    };
 private:
     asio::io_context io_context_;                   ///< ASIO I/O context for handling asynchronous network operations.
     asio::ip::udp::socket socket_;                  ///< ASIO UDP socket for sending and receiving packets.
@@ -160,6 +181,7 @@ private:
     std::array<uint8_t, 1400> recv_buffer_{};         ///< Buffer for receiving incoming packets.
     std::jthread thread;                            ///< Thread for running the ASIO I/O context.
     std::shared_ptr<std::map<GDTPMessageType, std::function<void(const GDTPHeader&, const std::vector<uint8_t>&, const asio::ip::udp::endpoint&)>>> message_handlers; ///< Handlers for processing received messages.
+    const std::shared_ptr<std::map<GDTPMessageType, std::function<std::vector<uint8_t>(std::vector<std::string>)>>> payload_handlers; ///< Handlers for preparing message payloads.
 
     /**
     * @brief Prepares the payload based on the GDTP message type and arguments.
@@ -167,165 +189,17 @@ private:
     * @param args The arguments required for the message's payload.
     * @return A vector of bytes representing the payload.
     */
-    static std::vector<uint8_t> preparePayload(
+    std::vector<uint8_t> preparePayload(
         const GDTPMessageType messageType,
         const std::vector<std::string>& args
     ) {
         std::vector<uint8_t> payload;
-        switch (messageType) {
-            case GDTPMessageType::PlayerMovement:
-                payload = preparePlayerMovement(args);
-                break;
-            case GDTPMessageType::PlayerShoot:
-                payload = preparePlayerShoot(args);
-                break;
-            case GDTPMessageType::ChatMessage:
-                payload = prepareChatMessage(args);
-                break;
-            case GDTPMessageType::ConnectionRequest: // Ajout de la gestion de la requête de connexion
-                payload = prepareConnectionRequest(args);
-                break;
-            // Ajout d'autres cas pour d'autres types de message
-            default:
-                std::cerr << "Message type not implemented!" << std::endl;
-                break;
+        if (this->payload_handlers->contains(messageType)) {
+            payload = (*this->payload_handlers)[messageType](args);
+        } else {
+            throw UnknownPayloadMessage(messageType);
+
         }
-        return payload;
-    }
-
-    /**
-     * @brief Prepares the payload for an error message (ErrorMessage).
-     * @param args The arguments required for the error message (error code and description).
-     * @return A vector of bytes representing the error message payload.
-     */
-    static std::vector<uint8_t> prepareErrorMessage(
-        const std::vector<std::string>& args
-    ) {
-        if (args.size() < 2) {
-            std::cerr << "Invalid arguments for ErrorMessage!" << std::endl;
-            return {};
-        }
-
-        const auto errorCode = static_cast<uint8_t>(std::stoi(args[0]));
-
-        const std::string& errorDescription = args[1];
-
-        std::vector<uint8_t> payload(1 + errorDescription.size());
-
-        payload[0] = errorCode;
-
-        std::memcpy(&payload[1], errorDescription.data(), errorDescription.size());
-
-        return payload;
-    }
-
-    /**
-     * @brief Prepares the payload for a connection request (ConnectionRequest).
-     * @param args The arguments required for the connection request (username).
-     * @return A vector of bytes representing the connection request payload.
-     */
-    static std::vector<uint8_t> prepareConnectionRequest(
-        const std::vector<std::string>& args
-    ) {
-        if (args.empty()) {
-            std::cerr << "Invalid arguments for Connection Request!" << std::endl;
-            return {};
-        }
-
-        std::string username = args[0];
-        std::vector<uint8_t> payload(username.begin(), username.end());  // Conversion du nom d'utilisateur en vecteur d'octets
-
-        return payload;
-    }
-
-    /**
-     * @brief Prepares the payload for a player movement message (PlayerMovement).
-     * @param args The arguments required for the player movement (Player ID, X, Y, Z coordinates).
-     * @return A vector of bytes representing the player movement payload.
-     */
-    static std::vector<uint8_t> preparePlayerMovement(
-        const std::vector<std::string>& args
-    ) {
-        if (args.size() < 4) {
-            std::cerr << "Invalid arguments for Player Movement!" << std::endl;
-            return {};
-        }
-
-        std::vector<uint8_t> payload;
-
-        const uint32_t playerId = htonl(std::stoi(args[0]));
-        const float x = std::stof(args[1]);
-        const float y = std::stof(args[2]);
-        const float z = std::stof(args[3]);
-
-        // Conversion des floats en format réseau
-        uint32_t x_net, y_net, z_net;
-        std::memcpy(&x_net, &x, 4);
-        x_net = htonl(x_net);
-        std::memcpy(&y_net, &y, 4);
-        y_net = htonl(y_net);
-        std::memcpy(&z_net, &z, 4);
-        z_net = htonl(z_net);
-
-        // Ajout des données au payload
-        payload.resize(16);
-        std::memcpy(&payload[0], &playerId, 4);
-        std::memcpy(&payload[4], &x_net, 4);
-        std::memcpy(&payload[8], &y_net, 4);
-        std::memcpy(&payload[12], &z_net, 4);
-
-        return payload;
-    }
-
-    /**
-     * @brief Prepares the payload for a player shoot message (PlayerShoot).
-     * @param args The arguments required for the player shoot (Player ID, direction, weapon type).
-     * @return A vector of bytes representing the player shoot payload.
-     */
-    static std::vector<uint8_t> preparePlayerShoot(
-        const std::vector<std::string>& args
-    ) {
-        if (args.size() < 3) {
-            std::cerr << "Invalid arguments for Player Shoot!" << std::endl;
-            return {};
-        }
-
-        std::vector<uint8_t> payload(6); // Player ID (4) + Direction (1) + Weapon type (1)
-
-        const uint32_t playerId = htonl(std::stoi(args[0]));
-        const auto direction = static_cast<uint8_t>(std::stoi(args[1]));
-        const auto weaponType = static_cast<uint8_t>(std::stoi(args[2]));
-
-        std::memcpy(&payload[0], &playerId, 4);
-        payload[4] = direction;
-        payload[5] = weaponType;
-
-        return payload;
-    }
-
-    /**
-     * @brief Prepares the payload for a chat message (ChatMessage).
-     * @param args The arguments required for the chat message (Player ID, message).
-     * @return A vector of bytes representing the chat message payload.
-     */
-    static std::vector<uint8_t> prepareChatMessage(
-        const std::vector<std::string>& args
-    ) {
-        if (args.size() < 2) {
-            std::cerr << "Invalid arguments for Chat Message!" << std::endl;
-            return {};
-        }
-
-        const uint32_t playerId = htonl(std::stoi(args[0]));
-        const std::string& message = args[1];
-        const auto messageLength = static_cast<uint16_t>(message.size());
-        const uint16_t messageLengthNetworkOrder = htons(messageLength);
-
-        std::vector<uint8_t> payload(6 + message.size());
-        std::memcpy(&payload[0], &playerId, 4);
-        std::memcpy(&payload[4], &messageLengthNetworkOrder, 2);
-        std::memcpy(&payload[6], message.data(), message.size());
-
         return payload;
     }
 
