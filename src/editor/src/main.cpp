9f41427652a82705bcdd396168f856ae43eb9869
LouisLanganay <louislanganay@gmail.com>
2024-09-24 18:08:45 +0200

    feat(main): add window initialization and exception
     initialization and exception handling to the function in create a window for the R-Type Map Editor and handle any exceptions that may occur during the execution of the program.

--- src/editor/src/main.cpp
+++ src/editor/src/main.cpp
@@ -1,7 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** main
+*/
+
+#include "Window/Window.hpp"
+#include "Exceptions.hpp"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Louis mais vreeeeeuuumant" << std::endl;
+int main() {
+    try {
+        Editor::Window window("R-Type Map Editor");
+
+        window.run();
+    } catch (const Editor::Exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
+    }
     return 0;
 }
