952aea01c778065c1cbb5e2f30333c554ee30ad3
LouisLanganay <louislanganay@gmail.com>
2024-09-24 18:06:09 +0200

    feat(EventManager): add EventManager class
    Add the EventManager class to handle events in the event types The is used to handle incoming events the registered callbacks.

---
+++ src/editor/src/EventManager/EventManager.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** editor
+** File description:
+** EventManager
+*/
+
+#ifndef EVENTMANAGER_HPP_
+#define EVENTMANAGER_HPP_
+
+#include <SFML/Window.hpp>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+namespace Editor {
+    class EventManager {
+        public:
+            using EventCallback = std::function<void(const sf::Event&)>;
+
+            void registerEvent(sf::Event::EventType type, EventCallback callback);
+            void handleEvent(const sf::Event &event) const;
+
+        private:
+            std::unordered_map<sf::Event::EventType, std::vector<EventCallback>> _callbacks;
+    };
+}
+
+#endif /* !EVENTMANAGER_HPP_ */
