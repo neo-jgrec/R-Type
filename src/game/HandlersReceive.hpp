67dc448b35b7c1eedf3bd3d67c253d4570faf257
tom <tom.lefoix@epitech.eu>
2024-09-24 16:35:34 +0200

    fix(feature/nertworking-service): compilation warning

--- src/game/HandlersReceive.hpp
+++ src/game/HandlersReceive.hpp
@@ -23,11 +23,6 @@ namespace Receive {
         }
         std::string username(payload.begin(), payload.end());
         std::cout << "Received Connection Request from username: " << username << std::endl;
-        // 3. Récupérer l'IP et le port du client
-        std::string client_ip = client_endpoint.address().to_string();
-        uint16_t client_port = client_endpoint.port();
-        std::cout << "Client IP: " << client_ip << ", Client Port: " << client_port << std::endl;
-        // 4. Envoyer une réponse de "Connection Accept" au client
     }
 
     void handleConnectionAccept([[maybe_unused]] const GDTPHeader &header, [[maybe_unused]] const std::vector<uint8_t>& payload, [[maybe_unused]] const asio::ip::udp::endpoint& client_endpoint)
