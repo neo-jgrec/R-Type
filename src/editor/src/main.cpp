81fbf264519ac3694418d5a50f9a9ef64ae971db
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-09-25 11:07:18 +0200

    build(editor): add imgui-sfml

--- src/editor/src/main.cpp
+++ src/editor/src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Louis mais vreeeeeuuumant" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Editor");
+
+    if (!ImGui::SFML::Init(window)) {
+        std::cerr << "Failed to initialize ImGui-SFML" << std::endl;
+        return -1;
+    }
+
+    sf::Clock deltaClock;
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
     return 0;
 }
