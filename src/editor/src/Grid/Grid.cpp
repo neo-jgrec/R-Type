4c26eab2fa072ab5f2bb5571e2c5d1f3646d40d9
LouisLanganay <louislanganay@gmail.com>
2024-09-25 11:18:28 +0200

    feat(grid): add ability to set grid size

--- src/editor/src/Grid/Grid.cpp
+++ src/editor/src/Grid/Grid.cpp
@@ -11,26 +11,7 @@ using namespace Editor;
 
 Grid::Grid(float width, float height, float cellSize)
     : _width(width), _height(height), _cellSize(cellSize) {
-    for (float x = 0; x <= width; x += cellSize) {
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, 0), sf::Color::White));
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, height), sf::Color::White));
-    }
-    for (float y = 0; y <= height; y += cellSize) {
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(0, y), sf::Color::White));
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(width, y), sf::Color::White));
-    }
-
-    _vertices.push_back(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
-    _vertices.push_back(sf::Vertex(sf::Vector2f(width, 0), sf::Color::Red));
-
-    _vertices.push_back(sf::Vertex(sf::Vector2f(width, height), sf::Color::Red));
-    _vertices.push_back(sf::Vertex(sf::Vector2f(0, height), sf::Color::Red));
-
-    _vertices.push_back(sf::Vertex(sf::Vector2f(0, height), sf::Color::Red));
-    _vertices.push_back(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
-
-    _vertices.push_back(sf::Vertex(sf::Vector2f(width, 0), sf::Color::Red));
-    _vertices.push_back(sf::Vertex(sf::Vector2f(width, height), sf::Color::Red));
+    defineGrid();
 }
 
 void Grid::draw(sf::RenderWindow &window) {
@@ -43,12 +24,39 @@ void Grid::updateGrid(float zoomLevel) {
     _cellSize = zoomLevel * 10;
     if (_cellSize < 5.0f)
         _cellSize = 5.0f;
+    defineGrid();
+}
+
+void Grid::setGridSize(int width, int height) {
+    _width = width;
+    _height = height;
+
+    defineGrid();
+}
+
+void Grid::defineGrid() {
+    sf::Color gridColor(255, 255, 255, 50);
+
+    _gridLines.clear();
     for (float x = 0; x <= _width; x += _cellSize) {
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, 0), sf::Color::White));
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, _height), sf::Color::White));
+        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, 0), gridColor));
+        _gridLines.push_back(sf::Vertex(sf::Vector2f(x, _height), gridColor));
     }
     for (float y = 0; y <= _height; y += _cellSize) {
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(0, y), sf::Color::White));
-        _gridLines.push_back(sf::Vertex(sf::Vector2f(_width, y), sf::Color::White));
+        _gridLines.push_back(sf::Vertex(sf::Vector2f(0, y), gridColor));
+        _gridLines.push_back(sf::Vertex(sf::Vector2f(_width, y), gridColor));
     }
+    _vertices.clear();
+
+    _vertices.push_back(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
+    _vertices.push_back(sf::Vertex(sf::Vector2f(_width, 0), sf::Color::Red));
+
+    _vertices.push_back(sf::Vertex(sf::Vector2f(_width, _height), sf::Color::Red));
+    _vertices.push_back(sf::Vertex(sf::Vector2f(0, _height), sf::Color::Red));
+
+    _vertices.push_back(sf::Vertex(sf::Vector2f(0, _height), sf::Color::Red));
+    _vertices.push_back(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
+
+    _vertices.push_back(sf::Vertex(sf::Vector2f(_width, 0), sf::Color::Red));
+    _vertices.push_back(sf::Vertex(sf::Vector2f(_width, _height), sf::Color::Red));
 }
--- src/editor/src/Grid/Grid.hpp
+++ src/editor/src/Grid/Grid.hpp
@@ -16,12 +16,16 @@ namespace Editor {
             Grid(float width, float height, float cellSize);
             void draw(sf::RenderWindow &window);
             void updateGrid(float zoomLevel);
+            void setGridSize(int width, int height);
+
         private:
             float _width;
             float _height;
             float _cellSize;
             std::vector<sf::Vertex> _vertices;
             std::vector<sf::Vertex> _gridLines;
+
+            void defineGrid();
     };
 }
 
