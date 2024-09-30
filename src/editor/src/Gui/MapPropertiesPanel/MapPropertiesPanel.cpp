6ce6aa86954111b2f6ab431a36cdac2ec4dc39f5
LouisLanganay <louislanganay@gmail.com>
2024-09-30 21:34:28 +0200

    feat(Gui/MapPanel): add MapPropertiesPanel class
    Add handling It includes functions setting the map name also includes notify when has changed.

---
+++ src/editor/src/Gui/MapPropertiesPanel/MapPropertiesPanel.cpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2024
+** editor
+** File description:
+** MapPropertiesPanel
+*/
+
+#include "MapPropertiesPanel.hpp"
+
+using namespace Editor;
+
+MapPropertiesPanel::MapPropertiesPanel() {}
+
+void MapPropertiesPanel::render(Map& map) {
+    ImGui::Begin("Map Properties");
+
+    static char mapName[256];
+    strncpy(mapName, map.getName().c_str(), sizeof(mapName));
+    mapName[sizeof(mapName) - 1] = '\0';
+    if (ImGui::InputText("Map Name", mapName, sizeof(mapName))) {
+        map.setName(mapName);
+        if (_onPropertyChanged) _onPropertyChanged();
+    }
+
+    int width = map.getWidth() / map.getGrid().getCellSize();
+    int height = map.getHeight() / map.getGrid().getCellSize();
+    int cellSize = map.getGrid().getCellSize();
+
+    if (ImGui::InputInt("Width", &width)) {
+        map.setWidth(width * cellSize);
+        if (_onPropertyChanged) _onPropertyChanged();
+    }
+    if (ImGui::InputInt("Height", &height)) {
+        map.setHeight(height * cellSize);
+        if (_onPropertyChanged) _onPropertyChanged();
+    }
+    if (ImGui::InputInt("Cell Size", &cellSize)) {
+        map.getGrid().setCellSize(cellSize);
+        if (_onPropertyChanged) _onPropertyChanged();
+    }
+
+    ImGui::End();
+}
+
+void MapPropertiesPanel::setOnPropertyChanged(std::function<void()> callback) {
+    _onPropertyChanged = std::move(callback);
+}
