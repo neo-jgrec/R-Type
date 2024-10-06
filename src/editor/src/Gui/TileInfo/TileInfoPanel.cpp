424de8fd9d17b60486f873d68c87f930ffced0ff
LouisLanganay <louislanganay@gmail.com>
2024-10-06 12:51:24 +0200

    refactor(Gui/TileInfoPanel): Remove unnecessary cout statement

--- src/editor/src/Gui/TileInfo/TileInfoPanel.cpp
+++ src/editor/src/Gui/TileInfo/TileInfoPanel.cpp
@@ -16,8 +16,6 @@ TileInfoPanel::TileInfoPanel() {}
 void TileInfoPanel::render(Map& map, std::vector<sf::Vector2i>& selectedTiles) {
     ImGui::Begin("Tile Information");
 
-    std::cout << "Selected Tiles: " << selectedTiles.size() << std::endl;
-
     for (const auto& selectedTile : selectedTiles) {
         ImGui::Text("Tile ID: %d", map.getTile(selectedTile.x, selectedTile.y));
     }
