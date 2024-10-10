8589df3754a1ce59242b4cb1d65fcb5b35c4896e
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-10-10 21:53:48 +0200

    fix(server): game engine logic set initWindow to false

--- src/server/src/Server/Server.hpp
+++ src/server/src/Server/Server.hpp
@@ -7,7 +7,7 @@
 
 class Server {
 private:
-    core::GameEngine _gameEngine;
+    core::GameEngine _gameEngine{false};
     NetworkingService _networkingService{12345};
 
     core::ecs::Entity _connectionHub;
