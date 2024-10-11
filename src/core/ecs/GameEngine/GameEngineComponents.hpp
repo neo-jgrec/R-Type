63b0166e5d3d6959b1c47ff5455ae06e823cc5a1
LouisLanganay <louislanganay@gmail.com>
2024-10-11 22:46:10 +0200

    feat(GameEngineComponents): Add new components for game entities

--- src/core/ecs/GameEngine/GameEngineComponents.hpp
+++ src/core/ecs/GameEngine/GameEngineComponents.hpp
@@ -11,6 +11,10 @@
 
 namespace core::ge {
 
+    /**
+     * @enum AnimationState
+     * @brief Represents different states of animation for game entities.
+     */
     enum class AnimationState {
         Moving,
         MovingUp,
@@ -18,6 +22,10 @@ namespace core::ge {
         Dying
     };
 
+    /**
+     * @struct KeyBinding
+     * @brief Defines key bindings for player input.
+     */
     struct KeyBinding {
         sf::Keyboard::Key moveUpKey = sf::Keyboard::Up;
         sf::Keyboard::Key moveDownKey = sf::Keyboard::Down;
@@ -26,6 +34,10 @@ namespace core::ge {
         sf::Keyboard::Key fireKey = sf::Keyboard::Space;
     };
 
+    /**
+     * @struct InputStateComponent
+     * @brief Represents the current input state for an entity.
+     */
     struct InputStateComponent {
         bool up = false;
         bool down = false;
@@ -34,15 +46,27 @@ namespace core::ge {
         bool fire = false;
     };
 
+    /**
+     * @struct DrawableComponent
+     * @brief Component for entities that can be drawn on screen.
+     */
     struct DrawableComponent {
         sf::RectangleShape shape;
     };
 
+    /**
+     * @struct ClockComponent
+     * @brief Component for tracking time-related information for an entity.
+     */
     struct ClockComponent {
         sf::Clock clock;
         sf::Time elapsedTime;
     };
 
+    /**
+     * @struct TransformComponent
+     * @brief Component for storing position, size, scale, and rotation of an entity.
+     */
     struct TransformComponent {
         sf::Vector2f position;
         sf::Vector2f size;
@@ -50,11 +74,19 @@ namespace core::ge {
         float rotation{};
     };
 
+    /**
+     * @struct ClickableComponent
+     * @brief Component for entities that can be clicked.
+     */
     struct ClickableComponent {
         bool clicked;
         std::function<void()> onClick;
     };
 
+    /**
+     * @struct AnimationComponent
+     * @brief Component for entities with animations.
+     */
     struct AnimationComponent {
         // std::vector<sf::IntRect> frames;
         std::map<AnimationState, std::vector<sf::IntRect>> animations;
@@ -65,10 +97,18 @@ namespace core::ge {
         bool loop = true;
     };
 
+    /**
+     * @struct ColorComponent
+     * @brief Component for storing color information of an entity.
+     */
     struct ColorComponent {
         sf::Color color;
     };
 
+    /**
+     * @struct SoundComponent
+     * @brief Component for entities that can play sounds.
+     */
     struct SoundComponent {
         sf::Sound sound;
         std::shared_ptr<sf::SoundBuffer> buffer;
@@ -76,20 +116,36 @@ namespace core::ge {
         bool isPlaying = false;
     };
 
+    /**
+     * @struct MusicComponent
+     * @brief Component for entities that can play music.
+     */
     struct MusicComponent {
         sf::Music music;
     };
 
+    /**
+     * @struct TextureComponent
+     * @brief Component for entities with textures.
+     */
     struct TextureComponent {
         std::shared_ptr<sf::Texture> texture;
     };
 
+    /**
+     * @struct CollisionComponent
+     * @brief Component for entities that can collide with others.
+     */
     struct CollisionComponent {
         uint32_t collisionMask = 0;
         std::vector<sf::FloatRect> collisionBoxes;
         std::vector<std::pair<uint32_t, std::function<void(const ecs::Entity&, const ecs::Entity&)>>> onCollision = {};
     };
 
+    /**
+     * @struct ButtonComponent
+     * @brief Component for button entities.
+     */
     struct ButtonComponent {
         sf::RectangleShape shape;
         std::function<void()> onClick;
@@ -97,15 +153,27 @@ namespace core::ge {
         bool isPressed = false;
     };
 
+    /**
+     * @struct TextComponent
+     * @brief Component for entities with text.
+     */
     struct TextComponent {
         sf::Text text;
         sf::Font font;
     };
 
+    /**
+     * @struct SceneComponent
+     * @brief Component for managing scene information.
+     */
     struct SceneComponent {
         int sceneName;
     };
 
+    /**
+     * @struct TextInputComponent
+     * @brief Component for text input fields.
+     */
     struct TextInputComponent {
         std::string text;
         std::string placeholder;
@@ -114,6 +182,10 @@ namespace core::ge {
         size_t maxLength = 100;
     };
 
+    /**
+     * @struct SliderComponent
+     * @brief Component for slider entities.
+     */
     struct SliderComponent {
         float minValue;
         float maxValue;
