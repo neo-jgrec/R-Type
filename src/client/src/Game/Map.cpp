016e55fec4e4b2577aef4fd502ff02b1b67dafa7
LouisLanganay <louis.langanay@epitech.eu>
2024-10-24 21:42:14 +0200

    fix(Game): Fix bug in Game::parseMap
    Fixed aparseMap function where the gameEngine.add instead of the function. This caused a runtime error when trying to access components later in the code.
    
    Changes:
    - Replaced all instances of gameEngine.add_component with gameEngine.registry.add_component

--- src/client/src/Game/Map.cpp
+++ src/client/src/Game/Map.cpp
@@ -165,32 +165,32 @@ void Game::parseMap(core::GameEngine& gameEngine, ConfigManager& config, const s
             tileShape.setTexture(tileTextures[tileIdx].get());
             tileShape.setTextureRect(tileRects[tileIdx]);
 
-            gameEngine.add_component(tileEntity, core::ge::TransformComponent{tilePos, {mapData["cellSize"].get<float>() * gameScale.x, mapData["cellSize"].get<float>() * gameScale.y}, {1.0f, 1.0f}, 0.0f});
-            gameEngine.add_component(tileEntity, core::ge::DrawableComponent{tileShape});
-            gameEngine.add_component(tileEntity, core::ge::TextureComponent{tileTextures[tileIdx]});
-            gameEngine.add_component(tileEntity, core::ge::SceneComponent{static_cast<int>(GameState::Playing)});
-            gameEngine.add_component(tileEntity, TileComponent{isDestructible});
+            gameEngine.registry.add_component(tileEntity, core::ge::TransformComponent{tilePos, {mapData["cellSize"].get<float>() * gameScale.x, mapData["cellSize"].get<float>() * gameScale.y}, {1.0f, 1.0f}, 0.0f});
+            gameEngine.registry.add_component(tileEntity, core::ge::DrawableComponent{tileShape});
+            gameEngine.registry.add_component(tileEntity, core::ge::TextureComponent{tileTextures[tileIdx]});
+            gameEngine.registry.add_component(tileEntity, core::ge::SceneComponent{static_cast<int>(GameState::Playing)});
+            gameEngine.registry.add_component(tileEntity, TileComponent{isDestructible});
 
-            gameEngine.add_component(tileEntity, core::ge::CollisionComponent{
+            gameEngine.registry.add_component(tileEntity, core::ge::CollisionComponent{
                 WORLD, {sf::FloatRect(0.0f, 0.0f, mapData["cellSize"].get<float>() * gameScale.x, mapData["cellSize"].get<float>() * gameScale.y)},
                 {
                     {PLAYER_PROJECTILE, [&](const core::ecs::Entity self, const core::ecs::Entity other) {
-                        const auto& tileOpt = gameEngine.get_components<TileComponent>()[self];
+                        const auto& tileOpt = gameEngine.registry.get_components<TileComponent>()[self];
                         if (tileOpt.has_value()) {
                             const auto& tile = tileOpt.value();
                             if (tile->isDestructible)
-                                gameEngine.remove_component<core::ge::DrawableComponent>(self);
+                                gameEngine.registry.remove_component<core::ge::DrawableComponent>(self);
                         } else {
                             std::cerr << "Error: TileComponent not found for entity." << std::endl;
                         }
-                        gameEngine.remove_component<core::ge::DrawableComponent>(other);
+                        gameEngine.registry.remove_component<core::ge::DrawableComponent>(other);
                     }},
                     {PLAYER_MISSILE, [&](const core::ecs::Entity self, [[maybe_unused]] const core::ecs::Entity other) {
-                        const auto& tileOpt = gameEngine.get_components<TileComponent>()[self];
+                        const auto& tileOpt = gameEngine.registry.get_components<TileComponent>()[self];
                         if (tileOpt.has_value()) {
                             const auto& tile = tileOpt.value();
                             if (tile->isDestructible)
-                                gameEngine.remove_component<core::ge::DrawableComponent>(self);
+                                gameEngine.registry.remove_component<core::ge::DrawableComponent>(self);
                         } else {
                             std::cerr << "Error: TileComponent not found for entity." << std::endl;
                         }
@@ -200,13 +200,13 @@ void Game::parseMap(core::GameEngine& gameEngine, ConfigManager& config, const s
                             auto& health = healthOpt.value();
                             health->health -= tileDamage;
                             if (health->health <= 0)
-                                gameEngine.remove_component<core::ge::DrawableComponent>(other);
+                                gameEngine.registry.remove_component<core::ge::DrawableComponent>(other);
                         } else {
                             std::cerr << "Error: HealthComponent not found for entity." << std::endl;
                         }
                     }},
                     {PLAYER, [&](const core::ecs::Entity, const core::ecs::Entity other) {
-                        auto disabled = gameEngine.get_component<core::ge::DisabledComponent>(other);
+                        auto disabled = gameEngine.registry.get_component<core::ge::DisabledComponent>(other);
                         disabled->disabled = true;
                     }},
                 }
