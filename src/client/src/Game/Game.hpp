38fd6d97875828f073171c2195236b9e9c72ade2
justineloizel <justine.loizel@gmail.com>
2024-10-26 15:56:28 +0200

    feat(feat/keybinds): variables to store key to update and texts to display

--- src/client/src/Game/Game.hpp
+++ src/client/src/Game/Game.hpp
@@ -76,6 +76,8 @@ public:
     GDTPHeader playerConnectionHeader{}; ///< Header for player connection requests.
     bool _windowOpen = true;           ///< Flag to track if the game window is open.
     sf::Vector2f gameScale = {1.0f, 1.0f}; ///< Scaling factor for the game view, adjusted during window resizing.
+    std::optional<std::string> keyToUpdate; ///< The key to update in the settings menu.
+    std::map<std::string, sf::Text> keyBindingTexts; ///< The key bindings for the game.
 
 private:
     /**
